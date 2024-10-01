81de01f37529f71a39f31dcbfd94a8d7bc076907
SPauly <spau@hotmail.de>
2024-10-01 17:06:45 +0200

    Fix setup script

--- build_setup.bat
+++ build_setup.bat
@@ -6,13 +6,20 @@ set ARG1=DUMP1090_FIRST_BUILD=ON
 echo Setting up build environment...
 
 echo Generating build files with CMake...
-cmake -G "%CMAKE_GENERATOR%" .
-
-echo Initiating setup build with: %ARG1%...
-cmake -S . -B build/
-cmake --build . --config Debug -D%ARG1%
-cmake --build . --config Release -D%ARG1%
+cmake -G "%CMAKE_GENERATOR%" -S . -B build/ -D%ARG1%
 
 echo Installing LibUSB...
-cmake --install build/third_party/libusb/ --config Release
+cmake --build build/third_party/libusb-cmake/ --config Debug
+cmake --build build/third_party/libusb-cmake/ --config Release
+cmake --install build/third_party/libusb-cmake/ --config Release
 
+echo Build and install pthreads-win32...
+cmake --build build/_deps/pthreads-win32-build/ --config Debug
+cmake --build build/_deps/pthreads-win32-build/ --config Release
+cmake --install build/_deps/pthreads-win32-build/ --config Release
+
+
+echo Starting regular build...
+cmake -S . -B build/ -DDUMP1090_FIRST_BUILD=OFF
+cmake --build build/ --config Debug
+cmake --build build/ --config Release
